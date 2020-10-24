cd462e4d06e567041fb606932ab4c1d99c14de75
Blaine Young <blaine.young3553@gmail.com>
2020-10-24 11:36:15 -0400

    updated partner router

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Partner = require('../models/partner');
 
 const partnerRouter = express.Router();
 
@@ -8,41 +9,69 @@ partnerRouter.use(bodyParser.json());
 partnerRouter.route('/')
 .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'application/json');
     next();
 })
 .get((req, res) => {
-    res.end('Will send all the partners to you');
+    Partner.find()
+    .then(partner => {
+        res.statusCode = 200;
+        res.json(partner)
+    }).catch(err => console.log(err))
 })
 .post((req, res) => {
-    res.end(`Will add the partner`);
+    Partner.create(req.body)
+    .then(partner => {
+        console.log('partner created: ', partner);
+        res.statusCode = 200;
+        res.json(partner);
+    }).catch(err => console.log(err))
 })
 .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /partners');
 })
 .delete((req, res) => {
-    res.end('Deleting all partners');
+    Partner.deleteMany()
+    .then(partner => {
+        res.statusCode = 200;
+        res.json(partner);
+    })
 });
 
 partnerRouter.route('/:partnerId')
 .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'application/json');
     next();
 })
 .get((req, res) => {
-    res.end(`Will send details of the partner: ${req.params.partnerId} to you`);
+    Partner.findById(req.params.partnerId)
+    .then(partner => {
+        res.statusCode = 200;
+        res.json(partner);
+    }).catch(err => console.log(err))
 })
 .post((req, res) => {
-    res.end(`Will add the partner: ${req.params.partnerId} `);
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.partnerId}`);
 })
 .put((req, res) => {
-    res.write(`Updating the partner: ${req.params.partnerId}\n`);
-    res.end(`Will update the partner: ${req.params.partnerId}`);
+    Partner.findByIdAndUpdate(req.params.partnerId, {
+        $set: req.body
+    }, { new: true })
+    .then(partner => {
+        res.statusCode = 200;
+        res.json(partner);
+    })
+    .catch(err => console.log(err));
 })
 .delete((req, res) => {
-    res.end(`Deleting partner: ${req.params.partnerId}`);
+    Partner.findByIdAndDelete(req.params.partnerId)
+    .then(partner => {
+        res.statusCode = 200;
+        res.json(partner)
+    }).catch(err => console.log(err))
 });
 
 module.exports = partnerRouter;
